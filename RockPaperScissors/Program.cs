20ff09b9dcdb5362ebb0c61ec69954af15fe938a
Michael Bond <michael@bondcodes.com>
2023-11-09 19:59:32 -0500

    Configure MudBlazor

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -1,5 +1,11 @@
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var services = builder.Services;
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+services
+    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddMudServices();
+
 await builder.Build().RunAsync();
